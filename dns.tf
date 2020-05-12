459fd81a1b3e836983e012b0a5797631369c4dc5
crazyinventor <d4vid81@gmail.com>
2020-05-12 15:53:45 +0200

    Adding stage, removing file

--- dns.tf
+++ dns.tf
@@ -63,3 +63,45 @@ resource "google_dns_record_set" "crazyinventor-net-webserver-www" {
     google_compute_instance.webserver
   ]
 }
+
+resource "google_dns_record_set" "crazyinventor-net-webserver-bigbro" {
+  name = "stage.bigbro.lab.${google_dns_managed_zone.crazyinventor-net.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.crazyinventor-net.name
+
+  rrdatas = [google_compute_instance.webserver.network_interface[0].access_config[0].nat_ip]
+
+  depends_on = [
+    google_compute_instance.webserver
+  ]
+}
+
+resource "google_dns_record_set" "crazyinventor-net-webserver-bigbro-api" {
+  name = "api.bigbro.lab.${google_dns_managed_zone.crazyinventor-net.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.crazyinventor-net.name
+
+  rrdatas = [google_compute_instance.webserver.network_interface[0].access_config[0].nat_ip]
+
+  depends_on = [
+    google_compute_instance.webserver
+  ]
+}
+
+resource "google_dns_record_set" "davidschneider-info-webserver-stage-www" {
+  name = "www.stage.${google_dns_managed_zone.davidschneider-info.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.davidschneider-info.name
+
+  rrdatas = [google_compute_instance.webserver.network_interface[0].access_config[0].nat_ip]
+
+  depends_on = [
+    google_compute_instance.webserver
+  ]
+}
