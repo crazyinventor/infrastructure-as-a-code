c8f624f91b3050895f48e8a15d32c93cfea1c445
crazyinventor <d4vid81@gmail.com>
2020-05-23 17:24:55 +0200

    Fixing firewall between web+db

--- firewall.tf
+++ firewall.tf
@@ -1,3 +1,28 @@
+resource "google_compute_firewall" "ansible" {
+  name    = "ansible"
+  network = google_compute_network.default-network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "database" {
+  name    = "database-firewall"
+  network = google_compute_network.default-network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3306"]
+  }
+
+  source_ranges = ["${google_compute_address.webserver-ip-address.address}/32"]
+  target_tags   = ["database"]
+}
+
 resource "google_compute_firewall" "webserver" {
   name    = "webserver-firewall"
   network = google_compute_network.default-network.name
@@ -9,16 +34,4 @@ resource "google_compute_firewall" "webserver" {
 
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["webserver"]
-}
-
-resource "google_compute_firewall" "ansible_webserver" {
-  name    = "ansible-firewall"
-  network = google_compute_network.default-network.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
 }
